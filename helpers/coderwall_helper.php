caeeb4c4520039fc185bdb52358a32de825e1a7c
Jan Lindblom <jan@nyfagel.se>
2012-12-02 13:31:59 +0100

    Basic stuff going in.

---
+++ helpers/coderwall_helper.php
@@ -0,0 +1,14 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * CodeIgniter Coderwall API Helper.
+ * 
+ * @package     helpers
+ * @author      Jan Lindblom <jan@nyfagel.se>
+ * @copyright   Copyright (c) 2012, Ny fågel.
+ * @license     MIT
+ * @version     0.0.1
+ */
+
+?>
+
